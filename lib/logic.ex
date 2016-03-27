ef8f6206ff7942a04cdca8720fd3c3df92e361fc
Lee Marlow <lee.marlow@gmail.com>
2016-03-27 17:10:14 -0600

    Add basic logic functions
    buffer/1
    not?/1
    and?/2
    nand?/2
    or?/2
    nor?/2
    xor?/2

--- lib/logic.ex
+++ lib/logic.ex
@@ -1,2 +1,24 @@
 defmodule Logic do
+  def buffer(in1)
+  def buffer(true), do: true
+  def buffer(false), do: false
+
+  def not?(in1)
+  def not?(true), do: false
+  def not?(false), do: true
+
+  def and?(in1, in2)
+  def and?(true, true), do: true
+  def and?(_, _), do: false
+
+  def nand?(in1, in2), do: and?(in1, in2) |> not?
+
+
+  def or?(in1, in2)
+  def or?(false, false), do: false
+  def or?(_, _), do: true
+
+  def nor?(in1, in2), do: or?(in1, in2) |> not?
+
+  def xor?(in1, in2), do: and?(or?(in1, in2), nand?(in1, in2))
 end
--- test/logic_test.exs
+++ test/logic_test.exs
@@ -1,11 +1,51 @@
 defmodule LogicTest do
+  alias Logic, as: L
+
   use ExUnit.Case
   doctest Logic
 
   test "buffer" do
-    assert buffer(true)
-    assert buffer(1)
-    refute buffer(false)
-    refute buffer(0)
+    assert L.buffer(true)
+    refute L.buffer(false)
+  end
+
+  test "not?" do
+    refute L.not?(true)
+    assert L.not?(false)
+  end
+
+  test "and?" do
+    assert L.and?(true, true)
+    refute L.and?(true, false)
+    refute L.and?(false, true)
+    refute L.and?(false, false)
+  end
+
+  test "nand?" do
+    refute L.nand?(true, true)
+    assert L.nand?(true, false)
+    assert L.nand?(false, true)
+    assert L.nand?(false, false)
+  end
+
+  test "or?" do
+    assert L.or?(true, true)
+    assert L.or?(true, false)
+    assert L.or?(false, true)
+    refute L.or?(false, false)
+  end
+
+  test "nor?" do
+    refute L.nor?(true, true)
+    refute L.nor?(true, false)
+    refute L.nor?(false, true)
+    assert L.nor?(false, false)
+  end
+
+  test "xor?" do
+    refute L.xor?(true, true)
+    assert L.xor?(true, false)
+    assert L.xor?(false, true)
+    refute L.xor?(false, false)
   end
 end

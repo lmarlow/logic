5f9cc363a2e55d0e0ad4b6da4f501da74437de62
Lee Marlow <lee.marlow@gmail.com>
2016-04-06 17:32:55 -0600

    Favor pattern matching over `if`s.
    Changed the name of `output_listeners` to `wires`.

--- lib/logic/gate.ex
+++ lib/logic/gate.ex
@@ -2,17 +2,17 @@ defmodule Logic.Gate do
   alias __MODULE__, as: G
   use GenServer
 
-  defstruct inputs: [], output_listeners: %{}, outputs_fn: nil
+  defstruct inputs: [], wires: %{}, outputs_fn: nil
 
   def start_link(inputs, outputs_fn) do
     GenServer.start_link(G, {inputs, outputs_fn})
   end
 
   def init({inputs, outputs_fn}) do
-    outputs = do_outputs(outputs_fn, inputs)
-    listeners = 1..length(outputs)
-                |> Enum.reduce(%{}, &(Map.put(&2, &1 - 1, [])))
-    {:ok, %G{inputs: inputs, outputs_fn: outputs_fn, output_listeners: listeners}}
+    outputs = inputs |> do_outputs(outputs_fn)
+    wires = 1..length(outputs)
+            |> Enum.reduce(%{}, &(Map.put(&2, &1 - 1, [])))
+    {:ok, %G{inputs: inputs, outputs_fn: outputs_fn, wires: wires}}
   end
 
   def input(gate_pid, position, value), do: input({gate_pid, position}, value)
@@ -32,44 +32,46 @@ defmodule Logic.Gate do
   end
 
   def handle_call(:inputs_and_outputs, _from, gate = %G{}) do
-    {:reply, {gate.inputs, do_outputs(gate.outputs_fn, gate.inputs)}, gate}
+    {:reply, {gate.inputs, gate.inputs |> do_outputs(gate.outputs_fn)}, gate}
   end
 
-  def handle_info({:input, _, _} = input, g), do: do_input(input, g)
+  def handle_info({:input, position, value} = input, g = %G{}) do
+    {:noreply, do_input(position, Enum.at(g.inputs, position), value, g)}
+  end
 
   def handle_cast({:connect, output_position,
                    other_gate, other_input},
                    gate = %G{}) do
     other = {other_gate, other_input}
-    input(other, do_outputs(gate.outputs_fn, gate.inputs)
+    input(other, gate.inputs |> do_outputs(gate.outputs_fn)
                  |> Enum.at(output_position)
          )
-    listeners = gate.output_listeners
-                |> Map.update(output_position, [other], &[other|&1])
-    {:noreply, %{gate | output_listeners: listeners}}
+    wires = gate.wires
+            |> Map.update(output_position, [other], &[other|&1])
+    {:noreply, %{gate | wires: wires}}
   end
 
-  defp do_input({:input, position, value},
-                gate = %G{inputs: ins,
-                          outputs_fn: outputs_fn,
-                          output_listeners: listeners}) do
-    if value != Enum.at(ins, position) do
-      new_ins = List.replace_at(ins, position, value)
-      outs = do_outputs(outputs_fn, ins)
-      new_outs = do_outputs(outputs_fn, new_ins)
-      if new_outs != outs do
-        outs
-        |> Enum.zip(new_outs)
-        |> Enum.with_index()
-        |> Enum.each(fn {{a, a}, _} -> nil
-                        {{_, b}, i} -> Map.get(listeners, i)
-                                       |> Enum.each(&(input(&1, b)))
-                     end)
-      end
-      gate = %{gate | inputs: new_ins}
-    end
-    {:noreply, gate}
+  defp do_input(position, old_value, new_value, gate)
+  defp do_input(_, v, v, g), do: g
+  defp do_input(p, _, value, g = %G{}) do
+    old_outs = g.inputs |> do_outputs(g.outputs_fn)
+    new_ins = g.inputs |> List.replace_at(p, value)
+    new_outs = new_ins |> do_outputs(g.outputs_fn)
+    g.wires |> send_outputs(old_outs, new_outs)
+    %{g | inputs: new_ins}
   end
 
-  defp do_outputs(fun, ins), do: apply(fun, ins)
+  defp send_outputs(_, a, a), do: nil
+  defp send_outputs(wires, old, new) do
+    old
+    |> Enum.zip(new)
+    |> Enum.with_index()
+    |> Enum.each(fn {{a, a}, _} -> nil
+                    {{_, b}, i} -> wires
+                                   |> Map.get(i)
+                                   |> Enum.each(&(input(&1, b)))
+                 end)
+  end
+
+  defp do_outputs(ins, fun), do: apply(fun, ins)
 end
